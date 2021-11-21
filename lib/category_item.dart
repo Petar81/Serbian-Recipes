a25e9888e60f7b4b5a819d60f1ccfcd462e9325b
Petar <baki_pfc@yahoo.com>
2021-11-22 00:41:31 +0100

    add navigation to each category item

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   const CategoryItem(this.title, this.color, {Key? key}) : super(key: key);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(builder: (_) {
+        return const CategoryMealsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Center(
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.subtitle1,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Center(
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
         ),
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: [color.withOpacity(0.7), color],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color.withOpacity(0.7), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
