f715329fcc5aefafcadcb98953e4d041523fc0fc
Petar <baki_pfc@yahoo.com>
2021-11-21 02:25:53 +0100

    make Categories Screen widget as the home widget

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,15 +8,20 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: dummyCategories
-          .map((catData) => CategoryItem(catData.title, catData.color))
-          .toList(),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deli Meals'),
+      ),
+      body: GridView(
+        children: dummyCategories
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
