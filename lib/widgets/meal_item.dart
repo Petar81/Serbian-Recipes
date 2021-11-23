b9e6468f63b51dacd5c6a9e9b5135b1af0336e81
Petar <baki_pfc@yahoo.com>
2021-11-23 23:49:41 +0100

    add title overlay to mael item image

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -46,6 +46,25 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
             ),
           ],
