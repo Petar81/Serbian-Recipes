e8c4e5db6b3ecd2c3f96579f1e4c87b7b70fd885
Petar <baki_pfc@yahoo.com>
2021-11-26 18:45:46 +0100

    add side drawer to the filters screen

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   const FiltersScreen({Key? key}) : super(key: key);
@@ -10,6 +12,7 @@ class FiltersScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Filters'),
       ),
+      drawer: const MainDrawer(),
       body: const Center(
         child: Text('Filters'),
       ),
